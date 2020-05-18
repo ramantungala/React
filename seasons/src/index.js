e2ffa337d038d728d63f5dd7be29eef3b44f4ebe
Ram Tungala <ramtungala@Rams-MacBook-Pro.local>
2020-05-18 18:57:32 +0100

    Section 4: Structuring Apps with Class-Based Components - Final commit in this section

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-    render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-          );
-        return <div>Latitude: </div>
+  constructor(props) {
+    super(props);
+    // THE ONLY PLACE TO DO DIRECT ASSIGNMENT
+    this.state = { lat: null, errorMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // setState should only be used to assign values into state
+        this.setState({ lat: position.coords.latitude });
+        // below statement is not valid as we are assigning value
+        // this.state = {lat: position.coords.latitude};
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  //React says we have to define render method, otherwise it throws error.
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
+  }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
